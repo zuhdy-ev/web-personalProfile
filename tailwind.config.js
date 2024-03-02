6154631d54ba0d9768948e56bda79932a8b2ce5e
zuhdy-ev <ardhiekov@gmail.com>
2024-03-02 07:48:43 +0700

    createNew: <page / welcome> build responsive UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
+    extend: {},
+    colors: {
+      primary: "#161A30",
+      secondary: "#31304D",
+      tertiary: "#B6BBC4",
+      fourth: "#F0ECE5",
+      fifth: "#39FF14",
     },
   },
   plugins: [],
-}
+};
